22668564ad02d602a33573b47442a6d0bc55b248
xEmpe <eemeli.mustonen15@gmail.com>
2025-02-05 18:58:01 +0200

    Finishing part1c

---
+++ practice/part1/src/AppAge.jsx
@@ -0,0 +1,30 @@
+/* const Hello = ({ name, age }) => {
+
+    const bornYear = () => new Date().getFullYear() - age
+  
+    return (
+      <div>
+        <p>
+          Hello {name}, you are {age} years old
+        </p>
+        <p>
+          So you were probably born {bornYear()}
+        </p>
+      </div>
+    )
+  }
+  
+  const App = () => {
+    const friends = [
+      { name: 'Leevi', age: 40 },
+      { name: 'Venla', age: 23 },
+    ]
+    return (
+      <div>
+        <Hello name={friends[0].name} age={friends[0].age}/>
+        <Hello name={friends[1].name} age={friends[1].age}/>
+      </div>
+    )
+  }
+  
+  export default App */
