0e63171735a705ee196db9bc1d11a0f63d4e3f32
xEmpe <eemeli.mustonen15@gmail.com>
2025-04-20 00:10:15 +0300

    Jatkuu

--- practice/backend/index.js
+++ practice/backend/index.js
@@ -17,7 +17,7 @@ const requestLogger = (request, response, next) => {
 
 app.use(requestLogger)
   
-  app.post('/api/notes', (request, response) => {
+  app.post('/api/notes', (request, response, next) => {
     const body = request.body
   
     if (!body.content) {
@@ -32,6 +32,7 @@ app.use(requestLogger)
     note.save().then(savedNote => {
       response.json(savedNote)
     })
+    .catch(error => next(error))
   })
   
   app.get('/', (request, response) => {
@@ -93,6 +94,8 @@ app.use(requestLogger)
   
     if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+      return response.status(400).json({error: error.message})
     }
   
     next(error)
