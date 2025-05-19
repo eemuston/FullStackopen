cbf622b7f4e495241a9840a47312cc79c058bd31
xEmpe <eemeli.mustonen15@gmail.com>
2025-05-19 12:57:34 +0300

    adding basic login and localstorage usage

--- osa4/bloglist-frontend/src/App.jsx
+++ osa4/bloglist-frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +15,81 @@ const App = () => {
     )  
   }, [])
 
+    useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      ) 
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+    console.log('logging in with', username, password)
+  }
+
+   const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>      
+  )
+
+    const handleLogout = () => {
+    console.log("clearing localstorage")
+    window.localStorage.clear()
+  }
+
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
+      {!user && loginForm()}
+      {user && <div>
+       <p>{user.name} logged in</p>
+       <form onSubmit={handleLogout} >
+        <button type="submit">log out</button>
+       </form>
+       <br/>
+         {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
-      )}
+        )}
+      </div>
+      } 
     </div>
   )
 }
