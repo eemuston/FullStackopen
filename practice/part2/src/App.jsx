e7394f5daecd1262429b01548c2e47d7bc5f7215
xEmpe <eemeli.mustonen15@gmail.com>
2025-05-20 11:10:05 +0300

    learning about ref

--- practice/part2/src/App.jsx
+++ practice/part2/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Note from "./components/Note"
 import noteService from './services/notes'
 import Notification from './components/Notification'
@@ -10,12 +10,12 @@ import NoteForm from './components/NoteForm'
 
 const App = () => {
   const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
   const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const noteFormRef = useRef()
 
   useEffect(() => {
     noteService
@@ -56,17 +56,12 @@ const App = () => {
     console.log('logging in with', username, password)
   }
 
-  const addNote = (event) => {
-    event.preventDefault()
-    const noteObject = {
-      content: newNote,
-      important: Math.random() > 0.5,
-    }
+  const addNote = (noteObject) => {
+    noteFormRef.current.toggleVisibility()
     noteService
     .create(noteObject)
     .then(returnedNote => {
       setNotes(notes.concat(returnedNote))
-      setNewNote('')
     })
   }
 
@@ -88,11 +83,6 @@ const App = () => {
         }, 5000)
         setNotes(notes.filter(n => n.id !== id))
       })
-  }
-
-  const handleNoteChange = (event) => {
-    //console.log(event.target.value)
-    setNewNote(event.target.value)
   }
 
   const notesToShow = showAll
@@ -124,12 +114,8 @@ const App = () => {
        <button onClick={handleLogout}>log out</button>
        <br/>
         {
-        <Togglable buttonLabel="new note">
-          <NoteForm
-            onSubmit={addNote}
-            value={newNote}
-            handleChange={handleNoteChange}
-          />
+        <Togglable buttonLabel="new note" ref={noteFormRef}>
+          <NoteForm createNote={addNote} />
         </Togglable>}
       </div>} 
       <div>
--- practice/part2/src/components/Togglable.jsx
+++ practice/part2/src/components/Togglable.jsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
+import { useState, useImperativeHandle, forwardRef } from 'react'
 
-const Togglable = (props) => {
+
+const Togglable = forwardRef((props, ref) => {
   const [visible, setVisible] = useState(false)
-  
+
   const hideWhenVisible = { display: visible ? 'none' : '' }
   const showWhenVisible = { display: visible ? '' : 'none' }
 
   const toggleVisibility = () => {
     setVisible(!visible)
   }
+
+
+  useImperativeHandle(ref, () => {
+    return {
+      toggleVisibility
+    }
+  })
 
   return (
     <div>
@@ -21,6 +29,7 @@ const Togglable = (props) => {
       </div>
     </div>
   )
-}
+
+})
 
 export default Togglable
