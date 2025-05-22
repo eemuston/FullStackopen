099899e6942cc4a74596be375fa250e97573f6fa
xEmpe <eemeli.mustonen15@gmail.com>
2025-05-22 18:15:30 +0300

    exercises next

--- practice/counter/src/App.jsx
+++ practice/counter/src/App.jsx
@@ -1,56 +1,12 @@
+import NewNote from "./components/NewNote"
+import Notes from "./components/Notes"
 
-const generateId = () =>
-  Number((Math.random() * 1000000).toFixed(0))
-
-const App = ({store}) => {
-
-  const createNote = (content) => { return {
-      type: 'NEW_NOTE',
-      payload: {
-        content,
-        important: false,
-        id: generateId()
-      }
-    }
-  }
-
-  const toggleImportanceOf = (id) => {
-    return {
-      type: 'TOGGLE_IMPORTANCE',
-      payload: { id }
-    }
-  }
-
-  const addNote = (event) => {
-    event.preventDefault()
-    const content = event.target.note.value
-    event.target.note.value = ''
-    store.dispatch(createNote(content))
-  }
-
-
-  const toggleImportance = (id) => {
-    store.dispatch(toggleImportanceOf(id))
-  }
+const App = () => {
 
   return (
     <div>
-
-      <form onSubmit={addNote}>
-        <input name="note" /> 
-        <button type="submit">add</button>
-      </form>
-      <ul>
-        {store.getState().map(note =>
-          <li
-            key={note.id} 
-
-            onClick={() => toggleImportance(note.id)}
-          >
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>
-        )}
-      </ul>
+      <NewNote />
+      <Notes />
     </div>
   )
 }
