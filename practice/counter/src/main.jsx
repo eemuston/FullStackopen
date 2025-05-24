be596722055db27d5ee43093538cfc2f62f75543
xEmpe <eemeli.mustonen15@gmail.com>
2025-05-24 12:15:34 +0300

    Creating filter for the redux notes app

--- practice/counter/src/main.jsx
+++ practice/counter/src/main.jsx
@@ -1,13 +1,21 @@
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 import App from './App'
 import noteReducer from './reducers/noteReducer'
+import filterReducer from './reducers/filterReducer'
 
-const store = createStore(noteReducer)
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer
+})
+
+const store = createStore(reducer)
+
+console.log(store.getState())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
-  </Provider>
+  </Provider>,
 )
--- practice/counter/src/reducers/noteReducer.jsx
+++ practice/counter/src/reducers/noteReducer.jsx
@@ -1,4 +1,14 @@
-const noteReducer = (state = [], action) => {
+const initialState = 
+{
+  notes: [
+    { content: 'reducer defines how redux store works', important: true, id: 1},
+    { content: 'state of store can contain any data', important: false, id: 2}
+  ],
+  filter: 'IMPORTANT'
+}
+
+const noteReducer = (state = initialState.notes, action) => {
+   console.log('ACTION: ', action)
    switch(action.type) {
     case 'NEW_NOTE':
       return [...state, action.payload]
