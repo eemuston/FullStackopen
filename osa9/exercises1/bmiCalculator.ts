fb8ceeea66320a087bef0f851ae01bfd152ef922
xEmpe <eemeli.mustonen15@gmail.com>
2025-09-12 19:53:21 +0300

    9.1 completed

---
+++ osa9/exercises1/bmiCalculator.ts
@@ -0,0 +1,20 @@
+const calculateBmi = (height: number, weight: number): string => {
+    const heightMeters = height / 100
+    const bmi = weight / (heightMeters * heightMeters)
+    const formatted = Math.round(bmi * 100) / 100
+
+    let range: string
+    if (bmi < 18.6)
+        range = "underweight"
+    else if(bmi < 25)
+        range = "normal"
+    else if(bmi < 30)
+        range = "overweight"
+    else if(bmi < 35)
+        range = "obese"
+    else
+        range = "extremely obese"
+    return `BMI with height of ${height} and weight of ${weight} is ${formatted}, which is in the ${range} range.`
+}
+
+console.log(calculateBmi(187, 90.7))
